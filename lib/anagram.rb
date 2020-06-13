f4ac605cd4ab262da3f60e0f665b1e063f2dd9c6
Robert Quintana <robertquintanatx@gmail.com>
2020-06-13 23:14:59 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,10 +6,9 @@ class Anagram
   end 
   
   def match(match_array)
-    match_array. do |maybe_match| 
-      maybe_array = maybe_match.split 
-      maybe_array = maybe_array.sort
-      
+    match_array.select do |maybe_match| 
+      maybe_array = maybe_match.split.sort 
+      maybe_array == @word.split.sort
     end 
   end 
   
